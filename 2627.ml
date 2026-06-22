6569aba2efe642280b3abbc716644064450bbf40
Koop Reynders <k.g.reynders@hva.nl>
2026-06-22 11:02:53 +0200

    Create 2627.ml with sprint schedule
    Added a schedule for sprints in the 2026-2027 academic year.

---
+++ 2627.ml
@@ -0,0 +1,51 @@
+# We ♥ Web - studiejaar 2026–2027
+Inspirerende en leerzame presentaties bij [FDND](https://www.fdnd.nl). Voor nerds, door nerds.
+
+## Sprint 1: Your Tribe, Sprint 13: Your Tribe for Life
+- Vrijdag 11 september, tbd.
+**tbd** over 
+
+## Sprint 2: The Client, Sprint 14: Loose Your Head
+- Vrijdag 2 oktober, tbd.
+**tbd** over 
+
+## Sprint 3: All Human, Sprint 15: Choices, Choices
+- Vrijdag 23 oktober, tbd.
+**tbd** over 
+
+## Sprint 4: Look and Feel, Sprint 16: Don't Repeat Yourself
+- Vrijdag 20 november, tbd.
+**tbd** over 
+
+## Sprint 5: Fix the Flow, Sprint 17: User Needs
+- Vrijdag 11 december, tbd. 
+**tbd** over 
+
+## Sprint 6: The Startup, Sprint 18: Release Candidate
+- Vrijdag 15 januari, tbd. 
+**tbd** over 
+
+## Sprint 7: Connect Your Tribe, Sprint 19: Free Space
+- Vrijdag 12 februari, tbd.
+**tbd** over 
+
+## Sprint 8: Server-Side Rendering, Sprint 19: Free Space
+- Vrijdag 5 maart, tbd.
+**tbd** over 
+
+## Sprint 9: The Web is for Everyone, Sprint 19: Free Space
+- Vrijdag 25 maart, tbd.
+**tbd** over 
+  
+## Sprint 10: User Experience, Sprint 20: Meesterproef
+- Vrijdag 16 april, tbd.
+**tbd** over 
+
+## Sprint 11: Pleasurable UI, Sprint 20: Meesterproef
+- Vrijdag 21 mei, tbd.
+**tbd** over 
+
+## Sprint 12: Proof of Concept, Sprint 20: Meesterproef
+- Vrijdag 11 juni, tbd.
+**tbd** over 
+
